d5077c5d81607cd2f67ddbff3cdc8c352c410ed7
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2023-11-14 17:16:55 -0300

    adicione algumas anotações

---
+++ JS basico para iniciantes/Aula-3/consolelog.js
@@ -0,0 +1,10 @@
+//A mais de um geito de atribuir uma string. 
+
+//Com '' pode ser colocado "" dentro dela sem dar erro
+console.log('Kaio "Benevenuto Hoffman"');// O ";" é opcional no JS 
+//Com "" pode ser colocado '' dentro dela também
+console.log("Kaio 'Benevenuto Hoffman'");
+//Com `` pode ser colocado tanto '' quanto "" sem quebrar as regras 
+console.log(`'Kaio' "Benevenuto Hoffman"`);
+
+console.log(100, 12.25, 'Kaio');
---
+++ JS basico para iniciantes/Aula-4/comentarios.js
@@ -0,0 +1,14 @@
+//Os comentários são trechos de código ignorados
+console.log("Olá mundo!"); //Não importa a posição
+console.log(1);
+//Em que coloque-os
+console.log(2)
+console.log(3)
+console.log(4)
+//Vão ser ignorados
+
+/*
+  Outro tipo 
+  de comentário 
+  é o de bloco
+*/
