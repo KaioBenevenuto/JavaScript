dbdcc9a95c1d54d70d49f87c37131397abd92c59
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2023-11-15 16:36:03 -0300

    adicionei anotações da aula 5

---
+++ JS basico para iniciantes/Aula-5/JS-mais-HTML/index.js
@@ -0,0 +1,4 @@
+//A tag Script proporciona a possibilidade de colocarmos códigos no HTML
+console.log("Olá mundo")
+console.log("Esse trecho será exibido no console do navegador")
+//Obs.: A função alert(''); não funciona no VScode, apenas no navegador
