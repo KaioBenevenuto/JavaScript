a60275ac13a87986f601bda113fafb8d6a04421a
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2023-11-21 18:02:57 -0300

    adicionei algumas anotações da aula 11

---
+++ JS basico para iniciantes/Aula-11/js/scriptExerc.js
@@ -0,0 +1,14 @@
+//alert - faz um alerta
+alert('Erro 402');
+
+//confirm - retorna uma valor boolean(true ou false)
+confirm('Deseja continuar?')
+
+//prompt - retorna uma string
+prompt('Digite seu nome')
+
+//---------------------- Exercício ----------------------
+const num1 = prompt('Digite um número:');
+const num2 = prompt('Digite outro número:');
+
+alert(`A soma é ${Number(num1) + Number(num2)}`)
