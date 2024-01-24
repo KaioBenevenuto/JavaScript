500bd325c954e556f00224983037f7c8d68ca75e
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2024-01-24 17:17:04 -0300

    atualizei o exercício da aula 26 - incompleto

--- Logica de programacao/Aula-26/modelo/assets/js/main.js
+++ Logica de programacao/Aula-26/modelo/assets/js/main.js
@@ -1,4 +1,26 @@
 function meuEscopo(){
-  //Aqui eu vou fazer o código
+  const peso = document.querySelector("#peso");
+  const altura = document.querySelector("#altura");
+  const IMC = (peso / altura) ** 2;
+  let resultado;
+
+  if (IMC < 18.5) {
+    resultado = `Seu IMC é ${IMC} (Abaixo do peso)`
+  } else if (IMC >= 18.5 || IMC <= 24.9) {
+    resultado = `Seu IMC é ${IMC} (Peso normal)`
+  } else if (IMC >= 25 || IMC <= 29.9) {
+    resultado = `Seu IMC é ${IMC} (Sobrepeso)`
+  } else if (IMC >= 30 || IMC <= 34.9) {
+    resultado = `Seu IMC é ${IMC} (	Obesidade de grau 1)`
+  } else if (IMC >= 35 || IMC <= 39.9) {
+    resultado = `Seu IMC é ${IMC} (	Obesidade de grau 2)`
+  } else if (IMC >= 40) {
+    resultado = `Seu IMC é ${IMC} (	Obesidade de grau 3)`
+  } else if (Number.isNAN(peso)) {
+    resultado = `Peso inválido`
+  } else if (Number.isNAN(altura)) {
+    resultado = `Altura inválida`
+  } 
+
 }
 meuEscopo()
