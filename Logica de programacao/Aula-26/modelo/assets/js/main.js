7a185da137684b7129ef035a8413773bfce0e595
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2024-01-28 12:02:19 -0300

    atualizei o exercício da aula 26 - incompleto

--- Logica de programacao/Aula-26/modelo/assets/js/main.js
+++ Logica de programacao/Aula-26/modelo/assets/js/main.js
@@ -1,26 +1,37 @@
 function meuEscopo(){
   const peso = document.querySelector("#peso");
   const altura = document.querySelector("#altura");
+  const form = document.querySelector("#form");
+  const resultado = document.querySelector("#resultado")
+
   const IMC = (peso / altura) ** 2;
-  let resultado;
+  let result;
+  
+  function recebeEventoForm(evento) {
+    evento.preventDefault()
+    
+    if (IMC < 18.5) {
+      result = `Seu IMC é ${IMC} (Abaixo do peso)`
+    } else if (IMC >= 18.5 || IMC <= 24.9) {
+      result = `Seu IMC é ${IMC} (Peso normal)`
+    } else if (IMC >= 25 || IMC <= 29.9) {
+      result = `Seu IMC é ${IMC} (Sobrepeso)`
+    } else if (IMC >= 30 || IMC <= 34.9) {
+      result = `Seu IMC é ${IMC} (Obesidade de grau 1)`
+    } else if (IMC >= 35 || IMC <= 39.9) {
+      result = `Seu IMC é ${IMC} (Obesidade de grau 2)`
+    } else if (IMC >= 40) {
+      result = `Seu IMC é ${IMC} (Obesidade de grau 3)`
+    } else if (Number.isNaN(peso)) {
+      result = `Peso inválido`
+    } else if (Number.isNaN(altura)) {
+      result = `Altura inválida`
+    }
+    /*Tenho que ver o que está acontecendo de errado no if para retornar só "undefined"*/
+    
+    resultado.innerHTML = result
+  }
 
-  if (IMC < 18.5) {
-    resultado = `Seu IMC é ${IMC} (Abaixo do peso)`
-  } else if (IMC >= 18.5 || IMC <= 24.9) {
-    resultado = `Seu IMC é ${IMC} (Peso normal)`
-  } else if (IMC >= 25 || IMC <= 29.9) {
-    resultado = `Seu IMC é ${IMC} (Sobrepeso)`
-  } else if (IMC >= 30 || IMC <= 34.9) {
-    resultado = `Seu IMC é ${IMC} (	Obesidade de grau 1)`
-  } else if (IMC >= 35 || IMC <= 39.9) {
-    resultado = `Seu IMC é ${IMC} (	Obesidade de grau 2)`
-  } else if (IMC >= 40) {
-    resultado = `Seu IMC é ${IMC} (	Obesidade de grau 3)`
-  } else if (Number.isNAN(peso)) {
-    resultado = `Peso inválido`
-  } else if (Number.isNAN(altura)) {
-    resultado = `Altura inválida`
-  } 
-
+  form.addEventListener("submit", recebeEventoForm)
 }
 meuEscopo()
