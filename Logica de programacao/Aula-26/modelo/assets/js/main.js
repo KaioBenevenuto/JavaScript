449a404fd038649337ce8e875ccc9c310274def6
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2024-01-31 16:38:08 -0300

    atualizei o exercício da aula 26 - incompleto

--- Logica de programacao/Aula-26/modelo/assets/js/main.js
+++ Logica de programacao/Aula-26/modelo/assets/js/main.js
@@ -1,34 +1,49 @@
-function meuEscopo(){
-  const peso = document.querySelector("#peso");
-  const altura = document.querySelector("#altura");
-  const form = document.querySelector("#form");
-  const resultado = document.querySelector("#resultado")
-
-  const IMC = (peso / altura) ** 2;
-  let result;
+function meuEscopo() {
+  const form = document.querySelector("#form")
   
   function recebeEventoForm(evento) {
     evento.preventDefault()
-    
-    if (IMC < 18.5) {
-      result = `Seu IMC é ${IMC} (Abaixo do peso)`
-    } else if (IMC >= 18.5 || IMC <= 24.9) {
-      result = `Seu IMC é ${IMC} (Peso normal)`
-    } else if (IMC >= 25 || IMC <= 29.9) {
-      result = `Seu IMC é ${IMC} (Sobrepeso)`
-    } else if (IMC >= 30 || IMC <= 34.9) {
-      result = `Seu IMC é ${IMC} (Obesidade de grau 1)`
-    } else if (IMC >= 35 || IMC <= 39.9) {
-      result = `Seu IMC é ${IMC} (Obesidade de grau 2)`
-    } else if (IMC >= 40) {
-      result = `Seu IMC é ${IMC} (Obesidade de grau 3)`
-    } else if (Number.isNaN(peso)) {
+
+    let peso = document.querySelector("#peso")
+    let altura = document.querySelector("#altura")
+    const resultado = document.querySelector("#resultado")
+    let result
+
+    let IMC = peso.value / altura.value ** 2
+    IMC = IMC.toFixed(2)
+
+    if ((peso.value > -1 && peso.value < 1) || isNaN(peso.value) || peso.value === "") {
+      resultado.style.backgroundColor = "crimson"
       result = `Peso inválido`
-    } else if (Number.isNaN(altura)) {
+    } else if ((altura.value > -1 && altura.value < 1) || isNaN(altura.value) || altura.value === "") {
+      resultado.style.backgroundColor = "crimson"
       result = `Altura inválida`
-    }
-    /*Tenho que ver o que está acontecendo de errado no if para retornar só "undefined"*/
-    
+    } else if (IMC < 18.5) {
+      resultado.style.backgroundColor = "aquamarine"
+      result = `Seu IMC é ${IMC} (Abaixo do peso)`
+    } else if (IMC >= 18.5 && IMC <= 24.9) {
+      resultado.style.backgroundColor = "aquamarine"
+      result = `Seu IMC é ${IMC} (Peso normal)`
+    } else if (IMC >= 25 && IMC <= 29.9) {
+      resultado.style.backgroundColor = "aquamarine"
+      result = `Seu IMC é ${IMC} (Sobrepeso)`
+    } else if (IMC >= 30 && IMC <= 34.9) {
+      resultado.style.backgroundColor = "aquamarine"
+      result = `Seu IMC é ${IMC} (Obesidade de grau 1)`
+    } else if (IMC >= 35 && IMC <= 39.9) {
+      resultado.style.backgroundColor = "aquamarine"
+      result = `Seu IMC é ${IMC} (Obesidade de grau 2)`
+    } else {
+      resultado.style.backgroundColor = "aquamarine"
+      result = `Seu IMC é ${IMC} (Obesidade de grau 3)`
+    } 
+
+    resultado.style.marginTop = "20px"
+    resultado.style.height = "50px"
+    resultado.style.borderRadius = "4px"
+    resultado.style.fontSize = "20px"
+    //Falta ainda centralizar a mensagem no meio
+
     resultado.innerHTML = result
   }
 
