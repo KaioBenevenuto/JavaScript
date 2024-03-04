7e7ed46e07639cd06bf3c653d804bd407a624716
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2024-03-04 16:56:34 -0300

    fiz uma pequena alteração

--- Logica de programacao/Aula-35/exercDeFor/assets/js/main.js
+++ Logica de programacao/Aula-35/exercDeFor/assets/js/main.js
@@ -1,10 +1,10 @@
 function meuEscopo() {
-  const elementos = [
-    { tag: "p", texto: "Frase 1" },
-    { tag: "div", texto: "Frase 2" },
-    { tag: "footer", texto: "Frase 3" },
-    { tag: "section", texto: "Frase 4" },
-  ]
+  // const elementos = [
+  //   { tag: "p", texto: "Frase 1" },
+  //   { tag: "div", texto: "Frase 2" },
+  //   { tag: "footer", texto: "Frase 3" },
+  //   { tag: "section", texto: "Frase 4" },
+  // ]
 
   function criaElementos() {
     const p = document.createElement("p")
