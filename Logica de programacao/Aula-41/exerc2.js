d0d0cc2b0b4cc27d246c3611398236592b5e95b8
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2024-03-16 10:21:25 -0300

    exerc2.js

---
+++ Logica de programacao/Aula-41/exerc2.js
@@ -0,0 +1,5 @@
+// Escreva uma função chamada ePaisagem que recebe dois argumenrtos largura e altura de uma imagem (number).
+// retorne true se a imagem estiver no modo paisagem
+
+const ePaisagem = (largura, altura) => (largura > altura);
+console.log(ePaisagem(1000, 800))
