b9d4364b5698c82e81911bded7845ccb1be2a070
Kaio Benevenuto Hoffman <kaiohoffmanbeneve@gmail.com>
2024-09-16 17:56:48 -0300

    adicionei as minhas anotações da aula

---
+++ Logica de programacao/Item-65/setInterval_e_setTimeout.js
@@ -0,0 +1,16 @@
+function mostraHora(data){
+  data = new Date()
+  return data.toLocaleTimeString("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  })
+}
+
+const timeOut = setInterval(function(){console.log(mostraHora())}, 500) 
+//setInterval - execulta uma função em um determinado intervalo de tempo
+//também da para colocar o valor gerado desse intervalo em uma váriavel
+
+setTimeout(function () {clearInterval(timeOut)}, 10000)
+//setTimeout - faz a mesma coisa só que, só uma vez
