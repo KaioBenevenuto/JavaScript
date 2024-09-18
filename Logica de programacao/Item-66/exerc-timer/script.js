94fb5cc9c0de0fabf2c4db7f78e13580c3779d14
Kaio Benevenuto Hoffman <kaiohoffmanbeneve@gmail.com>
2024-09-18 17:33:18 -0300

    fiz progresso no exercício mas ainda não terminei

--- Logica de programacao/Item-66/exerc-timer/script.js
+++ Logica de programacao/Item-66/exerc-timer/script.js
@@ -1,12 +1,37 @@
-document.getElementById("inicia").addEventListener("click", function () {
-  let timer = document.querySelector(".time")
-  timer = new Date()
-  timer.toLocaleTimeString("pt-BR", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
-  })
-  console.log(timer)
-})
+document.getElementById("inicia").addEventListener("click", iniciarTimer)
 
+function iniciarTimer(){
+  const timer = document.querySelector(".timer")
+
+  let h =23
+  let min =59
+  let seg =50
+
+  const timeOut = setInterval(function () {
+    seg++
+
+    if(seg === 60){
+      seg = 0
+      min++
+    }
+
+    if(min === 60){
+      min = 0
+      h++
+    }
+
+    if(h === 24){
+      stop(timeOut)
+    }
+
+    let hFormatted = h.toString().padStart(2, "0");
+    let minFormatted = min.toString().padStart(2, "0");
+    let segFormatted = seg.toString().padStart(2, "0");
+
+  timer.textContent = `${hFormatted}:${minFormatted}:${segFormatted}`
+}, 1000)
+}
+
+function stop(timeOut){
+  clearInterval(timeOut)
+}
