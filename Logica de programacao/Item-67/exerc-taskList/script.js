55b5fdf7800d68f391a0f4b1630d6af5f19283db
Kaio Benevenuto Hoffman <kaiohoffmanbeneve@gmail.com>
2024-10-23 17:24:34 -0300

    tirei o limite de linhas que podem ser adicionadas

--- Logica de programacao/Item-67/exerc-taskList/script.js
+++ Logica de programacao/Item-67/exerc-taskList/script.js
@@ -1,12 +1,11 @@
 function taskList(){
-  masterKey =0
   document.addEventListener('click', function(e){
     const elemento = e.target
-    const line = document.createElement("li")
-    const list = document.querySelector(".list")
     const name = document.querySelector(".nome")
 
-    if(elemento.classList.contains('add') && name.value != '' && masterKey < 3){
+    if(elemento.classList.contains('add') && name.value != ''){
+      const list = document.querySelector(".list")
+      const line = document.createElement("li")
       const apagar = document.createElement('button')
       apagar.classList.add('apagar')
 
@@ -16,12 +15,11 @@ function taskList(){
       line.appendChild(apagar)
       
       list.appendChild(line)
-      masterKey++
     }
 
     if(elemento.classList.contains('apagar')){
       elemento.parentElement.remove()
-      masterKey--
+      //isso faz com que apague o elemento pai 
     }
   })
 }
